e38cc7fc93151c79b1848c3d13ab15c750dcdbd1
Melvin Chia <melvinchia623600@gmail.com>
2026-01-05 11:57:06 +0800

    feat: update to latest version from forgistry

--- manifest.ts
+++ manifest.ts
@@ -2,13 +2,8 @@ import { lazy } from 'react'
 import type { ModuleConfig } from 'shared'
 
 export default {
-  name: 'Movies',
-  icon: 'tabler:movie',
   routes: {
     '/': lazy(() => import('@'))
   },
-  APIKeyAccess: {
-    tmdb: { required: true, usage: 'Fetch movie data from TMDB' }
-  },
-  category: 'Lifestyle'
+
 } satisfies ModuleConfig
